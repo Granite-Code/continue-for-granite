fbricon/continue

feat: show Granite Card in chat view until setup is complete
Signed-off-by: Fred Bricon <fbricon@gmail.com>

core/protocol/ideWebview.ts
@@ -96,4 +96,5 @@ export type ToWebviewFromIdeProtocol = ToWebviewFromIdeOrCoreProtocol & {
   exitEditMode: [undefined, void];
   focusEdit: [undefined, void];
   focusEditWithoutClear: [undefined, void];
+  showGraniteOnboardingCard: [boolean, void];
 };

Granite-Code/continue-for-granite

core/protocol/ideWebview.ts
@@ -96,4 +96,5 @@ export type ToWebviewFromIdeProtocol = ToWebviewFromIdeOrCoreProtocol & {
   exitEditMode: [undefined, void];
   focusEdit: [undefined, void];
   focusEditWithoutClear: [undefined, void];
+  setShowGraniteOnboardingCard: [boolean, void];
 };