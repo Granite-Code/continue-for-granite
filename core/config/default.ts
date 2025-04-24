865919411f6d99f12453e61645f4a0e8d3f07fab
Fred Bricon <fbricon@gmail.com>
2025-04-24 17:24:18 +0200

    Upgrade Granite models to 3.3
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- core/config/default.ts
+++ core/config/default.ts
@@ -1,7 +1,7 @@
 import {
-  AssistantUnrolled,
-  ConfigYaml,
-  ModelConfig,
+    AssistantUnrolled,
+    ConfigYaml,
+    ModelConfig,
 } from "@continuedev/config-yaml";
 
 export const defaultContextProvidersVsCode = [
@@ -48,9 +48,9 @@ const BASE_GRANITE_CONFIG: Partial<ModelConfig> = {
 };
 
 export const DEFAULT_MODEL_GRANITE_SMALL: ModelConfig = {
-  name: "granite3.2:2b",
+  name: "granite3.3:2b",
   provider: "ollama",
-  model: "granite3.2:2b",
+  model: "granite3.3:2b",
   ...BASE_GRANITE_CONFIG,
   defaultCompletionOptions: {
     ...BASE_GRANITE_CONFIG.defaultCompletionOptions,
@@ -59,9 +59,9 @@ export const DEFAULT_MODEL_GRANITE_SMALL: ModelConfig = {
 };
 
 export const DEFAULT_MODEL_GRANITE_LARGE: ModelConfig = {
-  name: "granite3.2:8b",
+  name: "granite3.3:8b",
   provider: "ollama",
-  model: "granite3.2:8b",
+  model: "granite3.3:8b",
   ...BASE_GRANITE_CONFIG,
   defaultCompletionOptions: {
     ...BASE_GRANITE_CONFIG.defaultCompletionOptions,
